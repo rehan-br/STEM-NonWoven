6b0532734eb248c1573d0534619b7ec05ec23d97
rehanbr <i200925@nu.edu.pk>
2025-02-18 23:03:58 +0500

    temp fix contact us

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
